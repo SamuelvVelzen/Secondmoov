193427b5f91b3d149b782f416c35335b2dadcbb7
Samuel <samuelvvelzen@gmail.com>
2021-01-03 18:52:55 +0100

    adding code to init slideshow

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,29 +1,41 @@
+// import {tns} from "./node_modules/tiny-slider/src/tiny-slider"
+import {tns} from './../../../../node_modules/tiny-slider/src/tiny-slider';
+
 export default {
     init() {
         // JavaScript to be fired on all pages
     },
     finalize() {
         // JavaScript to be fired on all pages, after page specific JS is fired
-        $('#partnerCarousel').carousel({
-            interval: 2000,
-        })
 
-        $('.carousel .carousel-item').each(function () {
-            var next = $(this).next();
-            if (!next.length) {
-                next = $(this).siblings(':first');
-            }
-            next.children(':first-child').clone().appendTo($(this));
-
-            for (let i = 0; i < 2; i++) {
-                next = next.next();
-                if (!next.length) {
-                    next = $(this).siblings(':first');
-                }
-
-                next.children(':first-child').clone().appendTo($(this));
-            }
-        });
-
+        if (document.getElementById('partnerCarousel')) {
+            tns({
+                container: '#partnerCarousel',
+                items: 3,
+                slideBy: 'page',
+                nav: false,
+                controls: false,
+                arrowKeys: true,
+                autoplay: true,
+                autoplayButtonOutput: false,
+                mouseDrag: true,
+                gutter: 20,
+                lazyload: true,
+                responsive: {
+                    0: {
+                        items: 2,
+                    },
+                    576: {
+                        items: 3,
+                    },
+                    768: {
+                        items: 4,
+                    },
+                    992: {
+                        items: 6,
+                    },
+                },
+            });
+        }
     },
 };
