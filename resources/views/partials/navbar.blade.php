453237459961730ca1e55c93b515179a55538f04
Samuel <samuelvvelzen@gmail.com>
2021-01-10 15:02:29 +0100

    improve styling and structure navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,20 +1,45 @@
-<header id="navbar">
-	<div class="container">
-		<a class="nav_brand" href="{{ home_url('/') }}">{{ get_bloginfo('name', 'display') }}</a>
-		<nav class="nav">
-			<ul class="nav_container">
-				@while(have_rows('menu','option')) @php(the_row())
+<div id="navbar-upper">
+	<div class="container d-flex align-items-center">
+		<a class="nav_brand" href="{{ home_url('/') }}">
+			<img class="nav_brand_logo" src="@asset('images/logo.png')">
+		</a>
+		@while(have_rows('contact','option')) @php(the_row())
+		@php($pageID = get_sub_field('url', false, false))
+		@if(get_sub_field('type') != "E-mail")
+			<div class="ml-auto contact_container d-flex align-items-center">
+				<img class="contact_container_logo" src="@asset('images/phone.png')">
+				<a class="contact_container_link d-flex flex-column"
+				   href="{{get_sub_field('type') =="E-mail" ? "mailto" : "tel"}}:{{the_sub_field('info')}}">
+					<p>Heb je een vraag?</p>
+					<p><b>Bel ons • </b>{{the_sub_field('info')}}</p>
+				</a>
+			</div>
+		@endif
+		@endwhile
+	</div>
+</div>
+
+<header id="navbar-lower">
+	<nav class="container navbar_container">
+		<ul class="navbar_group pl-0 d-flex list-unstyled mb-0 col-12 align-items-center">
+			@while(have_rows('menu','option'))
+				@php(the_row())
 				@php($pageID = get_sub_field('link', false, false))
 				@php($count++)
-				@if($count <= 2)
-					<li class="nav_container_item">
-						<a class="nav_container_item_link" href="{{get_the_permalink($pageID)}}">
+				@if($count <= 6)
+					<li class="navbar_group_item">
+						<a class="navbar_group_item_link" href="{{get_the_permalink($pageID)}}">
 							{{get_the_title($pageID)}}
 						</a>
 					</li>
 				@endif
-				@endwhile
-			</ul>
-		</nav>
-	</div>
+			@endwhile
+			@if(get_field('cta','option'))
+				@php($cta = get_field('cta', 'option'))
+				<a class="btn btn-primary ml-auto" href="{{$cta['link']}}">
+					{{$cta['label']}}
+				</a>
+			@endif
+		</ul>
+	</nav>
 </header>
