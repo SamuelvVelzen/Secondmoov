a3401ba77b1ccb2122ee0d0133bc9e19b4d3bffa
Samuel <samuelvvelzen@gmail.com>
2021-01-14 00:03:45 +0100

    improve layout file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,17 +4,15 @@
 <body @php body_class() @endphp>
 @php do_action('get_header') @endphp
 @include('partials.navbar')
-<main class="wrap container" role="document">
-	<div class="content">
-		<main class="main">
-			@yield('content')
-		</main>
-		@if (App\display_sidebar())
-			<aside class="sidebar">
-				@include('partials.sidebar')
-			</aside>
-		@endif
-	</div>
+<main class="wrap" role="document">
+
+	@yield('content')
+
+	{{--	@if (App\display_sidebar())--}}
+	{{--		<aside class="sidebar">--}}
+	{{--			@include('partials.sidebar')--}}
+	{{--		</aside>--}}
+	{{--	@endif--}}
 </main>
 @php do_action('get_footer') @endphp
 @include('partials.footer')
