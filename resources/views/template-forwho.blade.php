bd46da2648fdad3986b1d3c4a01a8510f1b1aebd
Samuel <samuelvvelzen@gmail.com>
2021-01-23 20:08:00 +0100

    adding structure for introduction

--- resources/views/template-forwho.blade.php
+++ resources/views/template-forwho.blade.php
@@ -6,19 +6,33 @@
 
 @section('content')
 	<div class="background small bg-tertiary"></div>
+	@if(get_field('intro_title') && get_field('intro_text') && get_field('intro_image'))
+		<div class="container">
+			<div class="row">
+				<h2 class="title col-12 col-md-6">{{the_field('intro_title')}}</h2>
+			</div>
+		</div>
+		<div class="container special">
+			<div class="row align-items-center">
+				<p class="col-12 col-md-6 font-weight-bold mb-4 mb-md-0">{{the_field('intro_text')}}</p>
+
+				<img class="logo col-12 col-md-5 offset-md-1" src="{{the_field('intro_image')}}">
+			</div>
+		</div>
+	@endif
 	<div class="container">
 		@php($forwhos = get_field('for_who'))
 		@if($forwhos)
 			@foreach($forwhos as $forwho)
 				<div class="row forwho-contents {{$loop->index % 2 == 1 ?'flex-row-reverse':''}}">
 					<div class="col-12">
-						<h2 class="title mb-0 {{$loop->first ? 'text-light' : ''}}  col-12 col-md-6">{{$forwho['for_who_content']['title']}}</h2>
+						<h2 class="title mb-0 col-12 col-md-6">{{$forwho['for_who_content']['title']}}</h2>
 					</div>
 					<div class="content col-12 col-md-6 {{$loop->index % 2 == 1 ?'offset-md-1':''}}">
-						<p class="introduction {{$loop->first ? 'text-light' : ''}} font-weight-bold">
+						<p class="introduction font-weight-bold text-content">
 							{{$forwho['for_who_content']['introduction']}}
 						</p>
-						<div class="text">
+						<div class="text text-content">
 							{!! $forwho['for_who_content']['text'] !!}
 						</div>
 					</div>
