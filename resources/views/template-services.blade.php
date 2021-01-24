220c469be695c15a485be91f00e607cf4b663368
Samuel <samuelvvelzen@gmail.com>
2021-01-24 20:48:32 +0100

    implemented new CPT

--- resources/views/template-services.blade.php
+++ resources/views/template-services.blade.php
@@ -4,6 +4,10 @@
 
 @extends('layouts.app')
 
+@php
+	$query = new WP_Query(array('post_type' => 'service', 'post_status' => 'publish','posts_per_page' => '-1'));
+@endphp
+
 @section('content')
 	<div class="background small bg-secondary"></div>
 	@if(get_field('intro_title') && get_field('intro_text') && get_field('intro_image'))
@@ -20,35 +24,39 @@
 			</div>
 		</div>
 	@endif
-	@php($services = get_field('services'))
-	@if($services)
+	@while(have_posts()) @php the_post() @endphp
+	@if($query->have_posts())
 		<div class="container">
-			@foreach($services as $service)
-				<div class="row">
-					<div class="col-12">
-						<h2 class="title mb-0 col-12 col-md-6">{{$service['title']}}</h2>
-					</div>
+			@php($i = 0)
+			@while($query->have_posts()) @php($query->the_post()) @php($i++)
+			<div class="row">
+				<div class="col-12">
+					<h2 class="title mb-0 col-12 col-md-6">{{the_title()}}</h2>
 				</div>
-				<div class="row {{$loop->index % 2 == 1 ?'flex-row-reverse':''}}">
-					<div class="service_text col-12 col-md-7 d-flex align-items-center {{$loop->index % 2 == 1 ?'offset-md-1':''}}">
-						<p class="font-weight-bold text-content">
-							{{$service['introduction']}}
-						</p>
-					</div>
-					<div class="service_image col-12 col-md-4 justify-content-center d-flex icon-img {{$loop->index % 2 == 0 ?'offset-md-1':''}}">
+			</div>
+			<div class="row {{$i % 2 == 1 ?'flex-row-reverse':''}}">
+				<div class="service_text col-12 col-md-7 d-flex align-items-center {{$i % 2 == 1 ?'offset-md-1':''}}">
+					<p class="font-weight-bold text-content">
+						{{the_field('introduction')}}
+					</p>
+				</div>
+				<div class="service_image col-12 col-md-4 justify-content-center d-flex icon-img {{$i % 2 == 0 ?'offset-md-1':''}}">
 						<span class="border_image">
 						<img class=""
-						     src="@asset('images/icon.png')"
+						     src="{{the_field('image')}}"
 						     alt="">
 							</span>
-					</div>
 				</div>
-				<div class="row mt-default-2">
-					<div class="text text-content">
-						{!! $service['text'] !!}
-					</div>
+			</div>
+			<div class="row mt-default-2">
+				<div class="text text-content">
+					{!! the_field('text') !!}
 				</div>
-			@endforeach
+			</div>
+			@endwhile
+			@php(wp_reset_postdata())
+		</div>
 		</div>
 	@endif
+	@endwhile()
 @endsection
