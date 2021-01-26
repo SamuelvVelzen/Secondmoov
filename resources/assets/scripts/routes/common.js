d1117b4dd34c62e7c23a3f477166fa83ec2c1971
Samuel <samuel@elevatedigital.nl>
2021-01-27 00:52:50 +0100

    add style to homepage and components

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -66,5 +66,50 @@ export default {
                 },
             });
         }
+
+        if (document.getElementById('appCarousel')) {
+            tns({
+                container: '#appCarousel',
+                items: 2,
+                slideBy: 'page',
+                nav: true,
+                navContainer: '#customize-nav-app',
+                controls: false,
+                arrowKeys: false,
+                autoplay: true,
+                autoplayButtonOutput: false,
+                mouseDrag: true,
+                gutter: 20,
+                lazyload: true,
+                responsive: {
+                    0: {
+                        items: 1,
+                    },
+                    576: {
+                        items: 2,
+                    },
+                    992: {
+                        items: 3,
+                        gutter: 40,
+                    },
+                },
+            });
+        }
+
+        if (document.getElementById('heroCarousel')) {
+            tns({
+                container: '#heroCarousel',
+                items: 1,
+                slideBy: 'page',
+                nav: true,
+                navContainer: '#customize-nav-hero',
+                controls: false,
+                arrowKeys: false,
+                autoplay: true,
+                autoplayButtonOutput: false,
+                mouseDrag: true,
+                lazyload: true,
+            });
+        }
     },
 };
