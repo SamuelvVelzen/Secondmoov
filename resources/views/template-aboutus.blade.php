bd377490f30218bec4b91e640ea770a5fb8430be
Samuel <samuelvvelzen@gmail.com>
2021-01-28 23:41:05 +0100

    added introduction component and added it to the different pages that uses the intro

--- resources/views/template-aboutus.blade.php
+++ resources/views/template-aboutus.blade.php
@@ -11,18 +11,7 @@
 @section('content')
 	<div class="background medium {{get_field('bg_color') ? the_field('bg_color') : 'bg-secondary'}}"></div>
 	@if(get_field('intro_title') && get_field('intro_text') && get_field('intro_image'))
-		<div class="container">
-			<div class="row">
-				<h2 class="title col-12 col-md-6">{{the_field('intro_title')}}</h2>
-			</div>
-		</div>
-		<div class="container special">
-			<div class="row align-items-center">
-				<p class="col-12 col-md-6 font-weight-bold mb-4 mb-md-0">{{the_field('intro_text')}}</p>
-
-				<img class="logo col-12 col-md-5 offset-md-1" src="{{the_field('intro_image')}}">
-			</div>
-		</div>
+		@include('components.introduction', ['title' => 'intro_title', 'text' => 'intro_text', 'image' => 'intro_image'])
 	@endif
 	@php($members = get_field('member_teams'))
 	@if($members)
--- resources/views/template-services.blade.php
+++ resources/views/template-services.blade.php
@@ -6,58 +6,48 @@
 
 @php
 	$bgColors = ['border-primary','border-secondary','border-tertiary','border-dark'];
-		$query = new WP_Query(array('post_type' => 'service', 'post_status' => 'publish','posts_per_page' => '-1'));
+	$query = new WP_Query(array('post_type' => 'service', 'post_status' => 'publish','posts_per_page' => '-1'));
 @endphp
 
 @section('content')
 	<div class="background small {{get_field('bg_color') ? the_field('bg_color') : 'bg-secondary'}}"></div>
 	@if(get_field('intro_title') && get_field('intro_text') && get_field('intro_image'))
-		<div class="container">
-			<div class="row">
-				<h2 class="title col-12 col-md-6">{{the_field('intro_title')}}</h2>
-			</div>
-		</div>
-		<div class="container special">
-			<div class="row align-items-center">
-				<p class="col-12 col-md-6 font-weight-bold mb-4 mb-md-0">{{the_field('intro_text')}}</p>
+		@include('components.introduction', ['title' => 'intro_title', 'text' => 'intro_text', 'image' => 'intro_image'])
 
-				<img class="logo col-12 col-md-5 offset-md-1" src="{{the_field('intro_image')}}">
-			</div>
-		</div>
-	@endif
-	@while(have_posts()) @php the_post() @endphp
-	@if($query->have_posts())
-		<div class="container">
-			@php($i = 0)
-			@while($query->have_posts()) @php($query->the_post()) @php($i++)
-			<div class="row">
-				<div class="col-12">
-					<h2 class="title mb-0 col-12 col-md-6 mb-default">{{the_title()}}</h2>
+		@while(have_posts()) @php the_post() @endphp
+		@if($query->have_posts())
+			<div class="container">
+				@php($i = 0)
+				@while($query->have_posts()) @php($query->the_post()) @php($i++)
+				<div class="row">
+					<div class="col-12">
+						<h2 class="title mb-0 col-12 col-md-6 mb-default">{{the_title()}}</h2>
+					</div>
 				</div>
-			</div>
-			<div class="row {{$i % 2 == 1 ?'flex-row-reverse':''}}">
-				<div class="service_text col-12 col-md-7 d-flex align-items-center {{$i % 2 == 1 ?'offset-md-1':''}}">
-					<p class="font-weight-bold text-content">
-						{{the_field('introduction')}}
-					</p>
-				</div>
-				<div class="service_image col-12 col-md-4 justify-content-center d-flex icon-img {{$i % 2 == 0 ?'offset-md-1':''}}">
+				<div class="row {{$i % 2 == 1 ?'flex-row-reverse':''}}">
+					<div class="service_text col-12 col-md-7 d-flex align-items-center {{$i % 2 == 1 ?'offset-md-1':''}}">
+						<p class="font-weight-bold text-content">
+							{{the_field('introduction')}}
+						</p>
+					</div>
+					<div class="service_image col-12 col-md-4 justify-content-center d-flex icon-img {{$i % 2 == 0 ?'offset-md-1':''}}">
 						<span class="border_image {{get_field('border_color') ? the_field('border_color') : $bgColors[rand(0, count($bgColors) - 1)] }}">
 						<img class=""
 						     src="{{the_field('image')}}"
 						     alt="">
 							</span>
+					</div>
 				</div>
-			</div>
-			<div class="row">
-				<div class="text text-content">
-					{!! the_field('text') !!}
+				<div class="row">
+					<div class="text text-content">
+						{!! the_field('text') !!}
+					</div>
 				</div>
+				@endwhile
+				@php(wp_reset_postdata())
 			</div>
-			@endwhile
-			@php(wp_reset_postdata())
-		</div>
-		</div>
+			</div>
+		@endif
+		@endwhile()
 	@endif
-	@endwhile()
 @endsection
