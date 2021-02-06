e71e1abe9639ceca4b77d95a797de8abed0877c4
Samuel <samuelvvelzen@gmail.com>
2021-02-06 18:16:51 +0100

    added structure

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,7 +10,7 @@
 				<img class="contact_container_logo square" src="@asset('images/phone.png')">
 				<a class="contact_container_link d-flex flex-column"
 				   href="{{get_sub_field('type') =="E-mail" ? "mailto" : "tel"}}:{{the_sub_field('info')}}">
-					<p>Heb je een vraag?</p>
+					<p class="d-none d-md-block">Heb je een vraag?</p>
 					<p><b>Bel ons • </b>{{the_sub_field('info')}}</p>
 				</a>
 			</div>
@@ -22,21 +22,58 @@
 <header id="navbar-lower" class="zindex-sticky">
 	<nav class="container navbar_container">
 		<ul class="navbar_group pl-0 d-flex list-unstyled mb-0 col-12 align-items-center">
+			@php($i = 0)
 			@while(have_rows('menu','option'))
 				@php(the_row())
 				@php($pageID = get_sub_field('link', false, false))
-				<li class="navbar_group_item">
-					<a class="navbar_group_item_link" href="{{get_the_permalink($pageID)}}">
-						{{get_the_title($pageID)}}
-					</a>
-				</li>
-			@endwhile
-			@if(get_field('cta','option'))
-				@php($cta = get_field('cta', 'option'))
-				<a class="btn btn-primary ml-auto" href="{{$cta['link']}}">
-					{{$cta['label']}}
-				</a>
-			@endif
+				@php($i++)
+				@if($i == 2)
+					<div class="w-100 d-md-flex d-none align-items-center">
+						@endif
+						<li class="navbar_group_item {{$i == 1 ? 'first' : null}}">
+							<a class="navbar_group_item_link" href="{{get_the_permalink($pageID)}}">
+								{{get_the_title($pageID)}}
+							</a>
+						</li>
+						@endwhile
+
+						@if(get_field('cta','option'))
+							@php($cta = get_field('cta', 'option'))
+							<a class="btn btn-primary ml-auto" href="{{$cta['link']}}">
+								{{$cta['label']}}
+							</a>
+						@endif
+					</div>
+
+					<div id="mobileButton" class="ml-auto">
+						<div class="d-block d-md-none mobileButton">
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+					</div>
 		</ul>
 	</nav>
 </header>
+
+<div id="mobileNavbarContainer">
+	<div class="mobileNavbarContainerInner">
+		@while(have_rows('menu','option'))
+			@php(the_row())
+			@php($pageID = get_sub_field('link', false, false))
+			<a class="navbar_group_item_link" href="{{get_the_permalink($pageID)}}">
+				<h3>{{get_the_title($pageID)}}</h3>
+			</a>
+		@endwhile
+
+		@if(get_field('cta','option'))
+			@php($cta = get_field('cta', 'option'))
+			<a class="btn btn-primary" href="{{$cta['link']}}">
+				{{$cta['label']}}
+			</a>
+		@endif
+	</div>
+</div>
