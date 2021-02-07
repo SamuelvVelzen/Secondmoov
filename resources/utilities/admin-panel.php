1812b9b70a7c8343d4d314049add384d3eade02f
Samuel <samuelvvelzen@gmail.com>
2021-02-07 14:17:46 +0100

    improve menu's

--- resources/utilities/admin-panel.php
+++ resources/utilities/admin-panel.php
@@ -1,8 +1,71 @@
 <?php
+	add_action('admin_menu', 'manage_menu', PHP_INT_MAX);
 
-// Add option pages (ACF)
+	function manage_menu()
+	{
+		$list = [
+			'samuel', 'admin'
+		];
+
+//		add seperator
+		add_seperator(7);
+
+//		add menu's
+		add_menu_page('Content', 'Content', 'read', 'content', '', 'dashicons-screenoptions', 5);
+		add_submenu_page(
+			'content',
+			'Berichten',
+			'Berichten',
+			'manage_options',
+			'edit.php'
+		);
+//		add_acf();
+
+//		remove menu's
+		remove_menu_page('edit.php');
+		remove_menu_page('edit-comments.php');
+
+		$current_user = wp_get_current_user();
+		if (!in_array($current_user->user_login, $list)) {
+			remove_menu_page('edit.php?post_type=acf-field-group');
+		}
+	}
+
+	add_action('admin_footer', 'visual_remove_default_template', 10);
+
+	function visual_remove_default_template()
+	{
+		global $pagenow;
+		if (in_array($pagenow, array('post-new.php', 'post.php')) && get_post_type() == 'page') { ?>
+			<script>
+                (function ($) {
+                    $(document).ready(function () {
+                        $('#page_template option[value="default"]').remove();
+                    })
+                })(jQuery)
+			</script>
+			<?php
+		}
+	}
+
+	function add_seperator($position)
+	{
+		global $menu;
+		$separator = [
+			0 => '',
+			1 => 'read',
+			2 => 'separator' . $position,
+			3 => '',
+			4 => 'wp-menu-separator'
+		];
+		if (isset($menu[$position])) {
+			$menu = array_splice($menu, $position, 0, $separator);
+		} else {
+			$menu[$position] = $separator;
+		}
+	}
+	
 	if (function_exists('acf_add_options_page')) {
-
 		// add parent
 		$parent = acf_add_options_page(array(
 			'page_title' => 'SecondMoove',
@@ -53,32 +116,3 @@
 			'parent_slug' => $parent['menu_slug'],
 		));
 	}
-
-// Hide ACF 'extra fields' in dashboard
-	add_action('admin_menu', function () {
-// List of users that don't have pages removed
-		$admins = [
-			'samuel',
-		];
-		$current_user = wp_get_current_user();
-		if (!in_array($current_user->user_login, $admins)) {
-			remove_menu_page('edit.php?post_type=acf-field-group');
-		}
-	}, PHP_INT_MAX);
-
-	function visual_remove_default_template()
-	{
-		global $pagenow;
-		if (in_array($pagenow, array('post-new.php', 'post.php')) && get_post_type() == 'page') { ?>
-			<script>
-                (function ($) {
-                    $(document).ready(function () {
-                        $('#page_template option[value="default"]').remove();
-                    })
-                })(jQuery)
-			</script>
-			<?php
-		}
-	}
-
-	add_action('admin_footer', 'visual_remove_default_template', 10);
