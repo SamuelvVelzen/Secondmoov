64838ceff2de8a5ceae3ad5e03ffc9b3370a0e32
Samuel <samuelvvelzen@gmail.com>
2021-02-07 21:09:54 +0100

    adding structure for relevant posts

--- resources/views/single.blade.php
+++ resources/views/single.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 
+@php
+	$query = new WP_Query(array('post_type' => 'post', 'post_status' => 'publish','posts_per_page' => '2', 'orderby' => 'date', 'order' => 'DESC'));
+@endphp
+
 @section('content')
-  @while(have_posts()) @php the_post() @endphp
-    @include('partials.content-single-'.get_post_type())
-  @endwhile
+	@while(have_posts()) @php the_post() @endphp
+	@include('partials.content-single-'.get_post_type(), array('query' => $query))
+	@endwhile
 @endsection
