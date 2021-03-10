d61db0df97965575115f5b6ab75a8f0ea23bbc4f
Samuel <samuel@elevatedigital.nl>
2021-03-10 23:47:49 +0100

    improve display of services

--- resources/views/template-services.blade.php
+++ resources/views/template-services.blade.php
@@ -16,30 +16,32 @@
 	@if($query->have_posts())
 		@while(have_posts()) @php the_post() @endphp
 		<div class="container">
-			@php($i = 0)
-			@while($query->have_posts()) @php($query->the_post()) @php($i++)
 			<div class="row">
 				<div class="col-12">
-					<h2 class="title mb-0 col-12 col-md-6 mb-default">{{the_title()}}</h2>
+					<h2 class="title col-12 col-md-6 mb-default">{{the_field('service_title')}}</h2>
 				</div>
 			</div>
-			<div class="row {{$i % 2 == 1 ?'flex-row-reverse':''}}">
-				<div class="service_text col-12 col-md-7 d-flex align-items-center {{$i % 2 == 1 ?'offset-md-1':''}}">
-					<p class="font-weight-bold text-content">
-						{{the_field('introduction')}}
-					</p>
-				</div>
-				<div class="service_image col-12 col-md-4 justify-content-center d-flex icon-img {{$i % 2 == 0 ?'offset-md-1':''}}">
+			@while($query->have_posts()) @php($query->the_post())
+			<div class="row mb-default-2">
+				<div class="service_image col-12 col-md-4 justify-content-center d-flex icon-img">
 						<span class="border_image {{get_field('border_color') ? the_field('border_color') : $bgColors[rand(0, count($bgColors) - 1)] }}">
 						<img class="service_img"
 						     src="{{the_field('image')}}"
 						     alt="">
 							</span>
 				</div>
-			</div>
-			<div class="row">
-				<div class="text text-content">
-					{!! the_field('text') !!}
+				<div class="service_text col-12 col-md-8 d-flex align-items-center flex-wrap">
+					<div class="col-12">
+						<h2 class="title col-12 col-md-6 mt-0 mb-default">{{the_title()}}</h2>
+					</div>
+
+					<p class="font-weight-bold text-content">
+						{{the_field('introduction')}}
+					</p>
+
+					<div class="text text-content">
+						{!! the_field('text') !!}
+					</div>
 				</div>
 			</div>
 			@endwhile
