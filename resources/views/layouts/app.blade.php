3155916a6f8ec89e7d57d3c57a87fb3b90fa0341
Samuel <samuel@elevatedigital.nl>
2021-03-13 21:37:41 +0100

    improve navbar sticky css and html

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,20 +2,22 @@
 <html {!! get_language_attributes() !!}>
 @include('partials.head')
 <body @php body_class() @endphp>
-@php do_action('get_header') @endphp
-@include('partials.navbar')
-<main class="wrap" role="document">
+<div>
+	@php do_action('get_header') @endphp
+	@include('partials.navbar')
+	<main class="wrap" role="document">
 
-	@yield('content')
+		@yield('content')
 
-	{{--	@if (App\display_sidebar())--}}
-	{{--		<aside class="sidebar">--}}
-	{{--			@include('partials.sidebar')--}}
-	{{--		</aside>--}}
-	{{--	@endif--}}
-</main>
-@php do_action('get_footer') @endphp
-@include('partials.footer')
-@php wp_footer() @endphp
+		{{--	@if (App\display_sidebar())--}}
+		{{--		<aside class="sidebar">--}}
+		{{--			@include('partials.sidebar')--}}
+		{{--		</aside>--}}
+		{{--	@endif--}}
+	</main>
+	@php do_action('get_footer') @endphp
+	@include('partials.footer')
+	@php wp_footer() @endphp
+</div>
 </body>
 </html>
