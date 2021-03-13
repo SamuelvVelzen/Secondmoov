5e2e60ce01df30947c1992b8f953a9d5cd47348a
Samuel <samuel@elevatedigital.nl>
2021-03-13 22:07:44 +0100

    improving introduction structure and styling

--- resources/views/components/introduction.blade.php
+++ resources/views/components/introduction.blade.php
@@ -1,9 +1,11 @@
-<div class="container">
-	<div class="row">
-		<h2 class="title col-12 col-md-6">{{the_field($title)}}</h2>
+<div class="container-fluid color-introduction {{get_field('bg_color') ? the_field('bg_color') : $defaultColor}}">
+	<div class="container">
+		<div class="row">
+			<h2 class="title col-12 {{get_field('bg_color') === 'bg-secondary' ? 'text-dark' : 'text-light'}}">{{the_field($title)}}</h2>
+		</div>
 	</div>
 </div>
-<div class="container special">
+<div class="container special pt-default">
 	<div class="row align-items-center">
 		<div class="col-12 col-md-6 font-weight-bold mb-4 mb-md-0">
 			{!! the_field($text) !!}
