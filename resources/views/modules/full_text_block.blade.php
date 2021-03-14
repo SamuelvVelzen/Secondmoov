92080e8e64dfbcaaf7b89793fd05525e5809dfc0
Samuel <samuel@elevatedigital.nl>
2021-03-14 23:46:33 +0100

    seperate flexible content in modules

---
+++ resources/views/modules/full_text_block.blade.php
@@ -0,0 +1,3 @@
+<div class="container mb-default mx-auto">
+	<div class="text-content">{!! the_sub_field('text') !!}</div>
+</div>
---
+++ resources/views/modules/image_block.blade.php
@@ -0,0 +1,3 @@
+<div class="container mb-default mx-auto">
+	<img src="{{the_sub_field('image')}}" alt="" class="w-100">
+</div>
